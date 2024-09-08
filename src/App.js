c7633b4d036c28aa305617d53412aa4afa73d555
kavipriya2004 <kavipriya.kousalya2004@gmail.com>
2024-09-08 12:21:51 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import FarmerDashboard from './components/FarmerDashboard';
+import BuyerDashboard from './components/BuyerDashboard';
+import Contracts from './components/Contracts';
+import Register from './components/Register';
+import ContractCreatePage from './components/ContractCreatePage';
+import ContractDetailsPage from './components/ContractDetailsPage';
+import ContractEditPage from './components/ContractEditPage';
+import { AuthProvider } from './AuthContext';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/farmer-dashboard" element={<FarmerDashboard />} />
+          <Route path="/buyer-dashboard" element={<BuyerDashboard />} />
+          <Route path="/contracts" element={<Contracts />} />
+          <Route path="/register" element={<Register />} /> 
+          <Route
+            path="/create-contract"
+            element={<ProtectedRoute element={ContractCreatePage} />}
+          />
+          <Route
+            path="/contract/:id"
+            element={<ProtectedRoute element={ContractDetailsPage} />}
+          />
+          <Route
+            path="/edit-contract/:id"
+            element={<ProtectedRoute element={ContractEditPage} />}
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
